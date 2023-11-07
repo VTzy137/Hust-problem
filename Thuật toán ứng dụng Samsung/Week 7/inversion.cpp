ad30596353d79052f65b49ebbab10e15bf0687da
VTzy <Truong.nv215495@sis.hust.edu.vn>
2023-11-07 20:49:31 +0700

    7/11/23

--- Thuật toán ứng dụng Samsung/Week 7/inversion.cpp
+++ Thuật toán ứng dụng Samsung/Week 7/inversion.cpp
@@ -1,18 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-int s[1000005], mod = 1e9+7, c = 0;
+int s[1000005], x[1000005], mod = 1e9+7, c = 0;
 void merge(int l, int m, int r){
 	if(r == l + 1)
 		return;
 	merge(l, (m+l)/2, m);
 	merge(m, (m+r)/2, r);
-	int j = l, i = m-1;
+	int k = l-1, j = l, i = m-1;
 	while(++i < r){
 		while(s[j] < s[i] && j < m)
-			++j;
-		c =  (c + j - l) % mod;
+			x[++k] = s[j++];
+		x[++k] = s[i];
+		c = (c + j - l) % mod;
 	}
-	sort(s+l, s+r);
+	while(j < m)
+		x[++k] = s[j++];
+	k = l - 1;
+	while(++k < r){
+		s[k] = x[k];
+	}
 }
 int main(){
 	int n;
