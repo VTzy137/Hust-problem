5b641f6cf563796c9fe0372e13c8179dcfbe2e8d
VTzy <Truong.nv215495@sis.hust.edu.vn>
2023-11-12 21:43:37 +0700

    12/11/23

--- Thuật toán ứng dụng Samsung/Week 7/TSP_large_scale.cpp
+++ Thuật toán ứng dụng Samsung/Week 7/TSP_large_scale.cpp
@@ -8,53 +8,56 @@ struct edges{
         c = c1;
     }
 };
-edges *s[500001];
-int ss[1001][1001], pa[1001]= {}, deep[1001] = {}, tmp, tmp1;
-bool vi[1001];
+int n, p = -1, k = -1, ci = 1;
+edges *s[500001]; // luu ds canh do dai tu nho den lon 
+int c[1001][1001] = {}, seq[1001] = {}; // luu cac canh va luu thu tu thanh pho tham
+bool vi[1001] = {};
 int main(){
-    int n, p = -1, k = -1, c = 1, x[1001] = {}, x1[1001] = {};
     cin >> n;
 	for(int i = 1; i <= n; i++){
 		for(int j = 1; j <= n; j++){
-			scanf("%d", &ss[i][j]);
-			if(i < j)
-				s[++p] = new edges(i, j, ss[i][j]);
+			scanf("%d", &c[i][j]);
+			if(i < j && n > 100)
+				s[++p] = new edges(i, j, c[i][j]);
 		}
 	}
 	if(n > 100){
+		int x[1001] = {}, x1[1001] = {}, pa[1001]= {}, deep[1001] = {};
+		// duyet duong di theo cay khung nho
 		sort(s, s + ++p, [](edges *a, edges *b)->bool{
 			return a->c < b->c;
 		});
-		while(c < n){
-			int i = s[++k]->i, j = s[k]->j, u = i, v = j, tmp = c;
+		while(ci < n){
+			int u = s[++k]->i, v = s[k]->j;
 			while(pa[u]) u = pa[u];
 			while(pa[v]) v = pa[v];
 			if(u != v){
+				int i = s[k]->i, j = s[k]->j, tmp = ci;
 				if(!x[i]){
 					if(!x[j]){
 						x[i] = j;
 						x[j] = i;
-						++c;
+						++ci;
 					}
 					else if(!x1[j]){
 						x[i] = j;
 						x1[j] = i;
-						++c;
+						++ci;
 					}
 				}
 				else if(!x1[i]){
 					if(!x[j]){
 						x1[i] = j;
 						x[j] = i;
-						++c;
+						++ci;
 					}
 					else if(!x1[j]){
 						x1[i] = j;
 						x1[j] = i;
-						++c;
+						++ci;
 					}
 				}
-				if(c > tmp){
+				if(ci > tmp){
 					if(deep[u] < deep[v]){
 						pa[u] = v;
 					}
@@ -75,12 +78,12 @@ int main(){
 			}
 		}
 		k = 0;
-		pa[0] = 1;
+		seq[0] = 1;
 		x1[v] = u;
 		x1[u] = v;
-		tmp = 1; tmp1 = x[1];
+		int tmp = 1, tmp1 = x[1];
 		while(tmp1 != 1){
-			pa[++k] = tmp1;
+			seq[++k] = tmp1;
 			if(x[tmp1] != tmp){
 				tmp = tmp1;
 				tmp1 = x[tmp1];
@@ -90,66 +93,113 @@ int main(){
 				tmp1 = x1[tmp1];
 			}
 		}
-		pa[++k] = 1;
+		seq[++k] = 1;
+		// thuc hien leo doi
+		int ll = 7;
+		for(int i = 0; i < 3 * n; i++){
+			int u = -1, v = 0, mx = 0, mn = 5000, co = 0;
+			for(int j = 0; j < n; j++){
+				if(!vi[seq[j]]){
+					++co; 
+					if(c[seq[j]][seq[j+1]] > mx){
+						u = j;
+						mx = c[seq[j]][seq[j+1]];
+					}
+				}
+			}
+			if(co < n / 3){
+				if(ll--){
+					for(int j = 0; j < 1001; j++)
+						vi[j] = 0;
+					continue;
+				}
+				else
+					break;
+			}
+			for(int j = 0; j < n && j != u; j++){
+				if(c[seq[u]][seq[j]] + c[seq[u+1]][seq[j+1]] <= mn){
+					v = j;
+					mn = c[seq[u]][seq[j]] + c[seq[u+1]][seq[j+1]];
+				}
+			}
+			vi[seq[u]] = 1;
+			if(v < u){
+				int tmp = v;
+				v = u;
+				u = tmp;
+			}
+			int kt = (v - u - 1) / 2;
+			for(int j = 0; j < kt; j++){
+				int tmp = seq[u + 1 + j];
+				seq[u + 1 + j] = seq[v - j];
+				seq[v - j] = tmp;
+			}
+		}
 	}
     else{
-        int visited[1005];
-        vector<int> result;
-        int current = 1;
-		pa[++k] = 1;
-        visited[current] = 1;
+		int best = 1e6, best_seq[105] = {};
+        bool visited[105] = {};
 
+        int current = 1;
+		seq[++k] = 1;
         while(1){
-            int next = -1;
-            int min_distance = INT_MAX;
-            for (int j = 1; j <= n; j++) {
-                if (!visited[j] && ss[current][j] < min_distance) {
-                    min_distance = ss[current][j];
+            int next = -1, min_distance = INT_MAX;
+            for(int j = 2; j <= n; j++){
+                if(!visited[j] && c[current][j] < min_distance){
+                    min_distance = c[current][j];
                     next = j;
                 }
             }
-            if (next == -1) {
+            if(next == -1)
                 break; 
-            }
-			pa[++k] = next;
+			seq[++k] = next;
             visited[next] = 1;
             current = next;
         }
-		pa[++k] = 1;
-    }
+		seq[++k] = 1;
 
-	for(int o = 0; o < 1000000 / n; ++o){
-		c = 0;
-		for(int i = 1; i < k; ++i){
-			if(ss[pa[i-1]][pa[i]] + ss[pa[i]][pa[i+1]] > c && !vi[i]){
-				p = i;
-				c = ss[pa[i-1]][pa[i]] + ss[pa[i]][pa[i+1]];
+		for(int o = 0; o < 10000; ++o){
+			if(o % 1000 == 0){
+				for(int i = 0; i < 101; ++i){
+					visited[o] = 1;
+				}
+			}
+			int cmin = 0;
+			p = -1;
+			for(int i = 1; i < n; ++i){
+				if(visited[seq[i]] && c[seq[i-1]][seq[i]] + c[seq[i]][seq[i+1]] > cmin){
+					p = i;
+					cmin = c[seq[i-1]][seq[i]] + c[seq[i]][seq[i+1]];
+				}
+			}
+			if(p == -1)
+				break;
+			int tmp = seq[p], j = p;
+			int tmp1 = c[seq[p-1]][tmp] + c[tmp][seq[p+1]] - c[seq[p-1]][seq[p+1]];
+			cmin = 1000;
+			// cout << "p:" << p << endl;
+			for(int i = p; i > 0; --i)
+				seq[i] = seq[i-1];
+			for(int i = 1; i < n; ++i){
+				seq[i-1] = seq[i];
+				if(c[seq[i-1]][tmp] + c[tmp][seq[i+1]] - c[seq[i-1]][seq[i+1]] < cmin){
+					cmin = c[seq[i-1]][tmp] + c[tmp][seq[i+1]] - c[seq[i-1]][seq[i+1]];
+					j = i;
+				}
+			}
+			if(tmp1 < cmin)
+				j = p;
+			for(int i = n - 1; i > j; --i){
+				seq[i] = seq[i-1];
+			}
+			seq[j] = tmp;
+			if(j == p){
+				visited[seq[j]] = 0;
 			}
 		}
-		vi[p] = true;
-		int j = 0;
-		tmp = pa[p];
-		for(int i = 1; i < k; ++i){
-			tmp1 = ss[pa[i-1]][pa[i]] + ss[pa[i]][pa[i+1]];
-			c += tmp1;
-			pa[p] = pa[i];
-			pa[i] = tmp;
-			if(ss[pa[i-1]][pa[i]] + ss[pa[i]][pa[i+1]] + ss[pa[p-1]][pa[p]] + ss[pa[p]][pa[p+1]] < c){
-				j = i;
-			}
-			pa[i] = pa[p];
-			pa[p] = tmp;
-			c -= tmp1;
-		}
-		if(j){
-			pa[p] = pa[j];
-			pa[j] = tmp;
-			vi[j] = false;
-			vi[p] = false;
-		}
 	}
-	cout << n << endl;
-	for(int i = 0; i < k; ++i){
-		cout << pa[i] << " ";
-	}
+	// in ket qua
+    cout << n << endl;
+	for(int i = 0; i <= n; ++i)
+		cout << seq[i] << " ";
 }
